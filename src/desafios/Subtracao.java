64e0d51d19e2e316ded65cc7247538a953cade71
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-25 18:10:52 -0300

    Desafio: subtração 10 menos 5.

---
+++ src/desafios/Subtracao.java
@@ -0,0 +1,15 @@
+/**
+ * 5. Crie uma classe Subtracao para imprimir o resultado de 10 - 5 no console.
+ */
+
+package desafios;
+
+public class Subtracao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int subtracao = a - b;
+
+        System.out.printf("%d - %d = %d", a, b, subtracao);
+    }
+}
