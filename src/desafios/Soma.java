77643b305955bf1a9036adcdb96758b3625d64c3
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-25 18:08:51 -0300

    Desafio: soma 5 mais 10.

---
+++ src/desafios/Soma.java
@@ -0,0 +1,17 @@
+/**
+ * 4. Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5.
+ *  Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor "15"
+ *  ao invés da expressão "10+5".
+ */
+
+package desafios;
+
+public class Soma {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+
+        System.out.printf("%d + %d = %d", a, b, soma);
+    }
+}
