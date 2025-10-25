98bf40a6b434d102e5312c1ea6602dddc93ed6ee
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-25 18:11:35 -0300

    Adicionando em pacote para organizar.

---
+++ src/exercicios/PrimeiraClasse.java
@@ -0,0 +1,14 @@
+/**
+ * Agora é com você! Faça o mesmo procedimento que eu fiz na aula,
+ * criando um projeto Java no IntelliJ que deve conter uma classe chamada
+ * PrimeiraClasse, com o método main, que ao executar deverá imprimir no console
+ * o texto “Concluí a aula 01 e agora estou mergulhando em Java!”.
+ */
+
+package exercicios;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
