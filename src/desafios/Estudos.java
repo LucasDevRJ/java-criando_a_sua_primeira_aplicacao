dd4285cf08f184377331c481716ae26062754f57
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-25 18:08:28 -0300

    Desafio: exibir anotações de estudos no terminal.

---
+++ src/desafios/Estudos.java
@@ -0,0 +1,21 @@
+/**
+ * 3. Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+ */
+
+package desafios;
+
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("--------------------|Anotações|--------------------");
+        System.out.println(
+                "Processo de compilação: converter códigos de alto nível para baixo" +
+                " nível, assim o computador irá conseguir entender o código e executa-lo."
+        );
+
+        System.out.println("Bytecode: arquivo compilado em um formato específico para" +
+                " passar instruções a JVM.");
+
+        System.out.println("JVM: recebe instruções do Bytecode para compilar o código" +
+                " Java em todas as plataformas possíveis.");
+    }
+}
