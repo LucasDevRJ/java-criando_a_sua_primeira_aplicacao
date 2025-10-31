90979ed4a349c3fab76be449edc39268151ff4e5
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-31 19:11:26 -0300

    Programa que concatena String com char.

---
+++ src/desafios/Concatenacao.java
@@ -0,0 +1,20 @@
+/*
+    Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra).
+     Atribua valores a essas variáveis e concatene-as em uma mensagem.
+ */
+package desafios;
+
+public class Concatenacao {
+    char letra;
+    String palavra;
+
+    public static void main(String[] args) {
+        Concatenacao concatenacao = new Concatenacao();
+        concatenacao.letra = 'L';
+        concatenacao.palavra = """
+                O nome 'Lucas' começa com %s.
+                """.formatted(concatenacao.letra);
+
+        System.out.println(concatenacao.palavra);
+    }
+}
