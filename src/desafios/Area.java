caa9c9d4b276e8097cea8b5a5a835ae764f38579
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-01 14:38:43 -0300

    Programa que calcula área do quadrado ou círculo.

---
+++ src/desafios/Area.java
@@ -0,0 +1,51 @@
+/**
+ * 3. Crie um menu que oferece duas opções ao usuário:
+ * "1. Calcular área do quadrado" e
+ * "2. Calcular área do círculo".
+ * Solicite a escolha do usuário e realize o cálculo da área com base na opção selecionada.
+ */
+
+package desafios;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        String menu = """
+                --------------------|MENU|--------------------
+                1 - Calcular área do quadrado
+                2 - Calcular área do círculo
+                ----------------------------------------------
+                """;
+        int opcao;
+        double area;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println(menu);
+        System.out.print("Digite a opção desejada: ");
+        opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.print("Digite o valor do lado: ");
+                double lado = entrada.nextDouble();
+
+                area = Math.pow(lado, 2);
+
+                System.out.printf("\nA área do quadrado equivale a %.2fcm²", area);
+                break;
+            case 2:
+                System.out.print("Digite o valor do raio: ");
+                double raio = entrada.nextDouble();
+
+                area = Math.PI * Math.pow(raio, 2);
+
+                System.out.printf("\nA área do círculo equivale a %.2fcm²", area);
+                break;
+
+            default:
+                System.out.println("Opção inválida.");
+        }
+    }
+}
--- src/desafios/DoisNumeros.java
+++ src/desafios/DoisNumeros.java
@@ -1,5 +1,5 @@
 /**
- * Peça ao usuário para inserir dois números inteiros.
+ * 2. Peça ao usuário para inserir dois números inteiros.
  * Compare os números e imprima uma mensagem indicando se são iguais,
  * diferentes, o primeiro é maior ou o segundo é maior.
  */
